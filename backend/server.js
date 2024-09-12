5521c01d65876e75dd5525cfe98d85e50a9a25e6
Yusuf Solak <yusuf_solak@outlook.com>
2024-09-12 09:48:33 +0300

    photos without cities

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ app.use("/images", express.static(imagesDir));
 
 // API endpoint to get photos with pagination
 app.get("/api/photos", (req, res) => {
-  const city = req.query.city; // Get the city from query parameters
+  const city = req.query.city.toLowerCase();
+   // Get the city from query parameters
+  console.log(city);
+  
   const page = parseInt(req.query.page) || 1;
   const limit = 10;
   const startIndex = (page - 1) * limit;
@@ -30,7 +33,7 @@ app.get("/api/photos", (req, res) => {
 
 
   if (!fs.existsSync(cityFolder)) {   // Check if the city folder exists
-    return res.status(404).json({ error: "City not found" });
+    return res.status(404).json({ error: "City not found" }+cityFolder);
   }
 
   fs.readdir(cityFolder, (err, files) => {
